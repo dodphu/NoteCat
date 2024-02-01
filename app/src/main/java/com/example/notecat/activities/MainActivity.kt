7c53b4bca08beb0ed9aebb4732388caa915708e1
dodphu <dodphu201@gmail.com>
2024-02-01 18:55:32 +0700

    add DAO interface

--- app/src/main/java/com/example/notecat/activities/MainActivity.kt
+++ app/src/main/java/com/example/notecat/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.notecat
+package com.example.notecat.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notecat.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/notecat/fragments/AllNoteFragment.kt
+++ app/src/main/java/com/example/notecat/fragments/AllNoteFragment.kt
@@ -1,4 +1,4 @@
-package com.example.notecat.fragment
+package com.example.notecat.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AddOrEditNoteFragment.newInstance] factory method to
+ * Use the [AllNoteFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AddOrEditNoteFragment : Fragment() {
+class AllNoteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class AddOrEditNoteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_or_edit_note, container, false)
+        return inflater.inflate(R.layout.fragment_all_note, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class AddOrEditNoteFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AddOrEditNoteFragment.
+         * @return A new instance of fragment AllNoteFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AddOrEditNoteFragment().apply {
+            AllNoteFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
